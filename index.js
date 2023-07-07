147560660579db80cef7f37a4eb0f0f5f6626ccc
alizeba11323 <alizeba11323@gmail.com>
2023-07-07 11:06:59 +0530

    add new route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 8000;
 app.get("/", function (req, res, next) {
   res.send({ hi: "There" });
 });
+app.get("/server-app", function (req, res, next) {
+  res.status(200).json({ message: "finally done" });
+});
 app.listen(PORT, function () {
   console.log("App running in port " + PORT);
 });
